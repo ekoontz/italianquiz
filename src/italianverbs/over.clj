de49e0197d04a4b291b3adb42a395d4edeeacbbc
Eugene Koontz <ekoontz@hiro-tan.org>
2013-12-24 23:59:17 -0800

    failed to push down over

--- src/italianverbs/over.clj
+++ src/italianverbs/over.clj
@@ -9,7 +9,6 @@
 
    [clojure.tools.logging :as log]
 
-   [italianverbs.generate :refer :all]
    [italianverbs.lexicon :refer :all]
    [italianverbs.lexiconfn :refer :all]
    [italianverbs.morphology :refer [finalize fo]]
@@ -40,138 +39,76 @@
 
    true (throw (Exception. (str "into-map: don't know what to do with a " (type arg) ".")))))
 
-(defn over-each-parent [parents child1 child2]
-  (if (not (empty? parents))
-    (let [parent (first parents)]
-      (log/debug (str "parent: " parent))
-      (lazy-cat
-       (cond (and (map? parent)
-                  (not (nil? (:serialized parent))))
-             ;; In this case, supposed 'parent' is really a lexical item: for now, definition of 'lexical item' is,
-             ;; it has a non-nil value for :serialized - just return nil, nothing else to do.
-             (throw (Exception. (str "Don't know what to do with this parent: " parent)))
-
-             (and (map? parent)
-                  (not (nil? (:schema parent))))
-                  ;; figure out whether head is child1 or child2:
-             (let [head
-                   (cond
-                    (= \c (nth (str (:schema parent)) 0))
-                    child2
-
-                    (= \h (nth (str (:schema parent)) 0))
-                    child1
-
-                    true
-                    (throw (Exception. (str "Don't know what the head-vs-complement ordering is for parent: " parent))))
-                   comp
-                   (if (= head child1)
-                     child2 child1)]
-               (generate (list parent)
-                         "parent" {:head head
-                                   :comp comp} sem-impl))
-
-             ;; if parent is a symbol, evaluate it; should evaluate to a list of expansions (which might also be symbols, etc).
-             (symbol? parent)
-             (over-each-parent (eval parent) child1 child2)
-
-             ;; if parent is map, do introspection: figure out the schema from the :schema-symbol attribute,
-             ;; and figure out head-comp ordering from :first attribute.
-             (and (map? parent)
-                  (not (nil? (:schema-symbol parent))))
-             (generate (list (eval (:schema-symbol parent)))
-                       "parent"
-                       {:head (if (= (:first parent) :head)
-                                child1 child2)
-                        :comp (if (= (:first parent) :head)
-                                child2 child1)}
-                       sem-impl)
-
-
-             true
-             (throw (Exception. (str "Don't know what to do with parent: " parent))))
-
-       (over-each-parent (rest parents) child1 child2)))))
-
-(defn over-each-child2 [parents child1s child2]
-  (if (not (empty? child1s))
-    (lazy-cat
-     (over-each-parent parents (first child1s) child2)
-     (over-each-child2 parents (rest child1s) child2))))
-
-(defn over-each-child1 [parents child1s child2s]
-  (if (not (empty? child2s))
-    (lazy-cat
-     (over-each-child2 parents child1s (first child2s))
-     (over-each-child1 parents child1s (rest child2s)))))
-
-(defn over-gen [parents child1 & [child2]]
-  (let [child1 (into-list-of-maps child1)
-        child2 (into-list-of-maps child2)]
-    (over-each-child1 (if (seq? parents)
-                        (shuffle parents)
-                        (list parents))
-                      child1 child2)))
-
 (defn mylazycat [one two]
   (lazy-cat one two))
 
 (declare overh)
 (declare overc)
 
-(defn over-each-parent-head [parents head]
-  (log/debug (str "over-each-parent-head: parents type: " (type parents)))
-  (log/debug (str "over-each-parent-head: head type: " (type head)))
-  (if (not (empty? parents))
-    (let [each-parent (first parents)]
-      (log/debug (str "over-each-parent-head: each-parent type:" (type (first parents))))
-      (log/debug (str "over-each-parent-head: head type:" (type head)))
-      (lazy-cat
-       (overh each-parent head)
-       (over-each-parent-head (rest parents) head)))
-    (do
-      (log/debug (str "over-each-parent-head: done. returning nil"))
-      nil)))
+(defn moreover-head [parent child lexfn-sem-impl]
+  (do
+    (log/debug (str "moreover-head (candidate) parent: " (fo parent)))
+    (log/debug (str "moreover-head (candidate) parent sem: " (get-in parent '(:synsem :sem) :no-semantics)))
+    (log/debug (str "moreover-head (candidate) head child sem:" (get-in child '(:synsem :sem) :top)))
+    (log/debug (str "moreover-head (candidate) head:" (fo child)))
+    (let [result
+          (unifyc parent
+                  (unifyc {:head child}
+                          {:head {:synsem {:sem (lexfn-sem-impl (get-in child '(:synsem :sem) :top))}}}))]
+      (if (not (fail? result))
+        (let [debug (log/debug (str "moreover-head " (get-in parent '(:comment)) " (SUCCESS) result sem: " (get-in result '(:synsem :sem))))
+              debug (log/debug (str "moreover-head (SUCCESS) parent (2x) sem: " (get-in parent '(:synsem :sem))))]
+          (merge {:head-filled true}
+                 result))
+        (let [debug (log/debug (str "moreover-head " (fo child) "/" (get-in parent '(:comment)) "," (fo child) "/" (get-in child '(:comment))))
+              fail-path (fail-path result)
+              debug (log/debug (str " fail-path: " fail-path))
+              debug (log/debug (str " path to head-value-at-fail:" (rest fail-path)))
+              debug (log/debug (str " head: " (fo child)))
+              debug (log/debug (str " head-value-at-fail: " (get-in child (rest fail-path))))
+              debug (log/debug (str " parent-value-at-fail: " (get-in parent fail-path)))]
+          (do
+            (log/debug (str "FAIL: " fail-path))
+            :fail))))))
 
-(defn over-each-parent-comp [parents comp]
-  (log/debug (str "over-each-parent-comp: parents type: " (type parents)))
-  (log/debug (str "over-each-parent-comp: comp type: " (type comp)))
-  (if (not (empty? parents))
-    (let [each-parent (first parents)]
-      (log/debug (str "over-each-parent-comp: each-parent type:" (type (first parents))))
-      (log/debug (str "over-each-parent-comp: comp type:" (type comp)))
-      (lazy-cat
-       (overc each-parent comp)
-       (over-each-parent-comp (rest parents) comp)))
-    (do
-      (log/debug (str "over-each-parent-comp: done. returning nil"))
-      nil)))
+;; Might be useful to set the following variable to true,
+;; if doing grammar development and it would be unexpected
+;; to have a failing result from calling (moreove-comp)
+;; with certain arguments.
+(def ^:dynamic *throw-exception-if-failed-to-add-complement* false)
 
-(defn over-each-head-child [parent children]
-  (log/debug (str "over-each-head-child: parent type: " (type parent)))
-  (log/debug (str "over-each-head-child: head children type: " (type children)))
-  (if (not (empty? children))
-    (let [each-child (first children)]
-      (log/debug (str "over-each-head-child: each-parent?: " (:comment (first children))))
-      (lazy-cat
-       (overh parent each-child)
-       (over-each-head-child parent (rest children))))
-    (do
-      (log/debug (str "over-each-head-child: done. returning nil."))
-      nil)))
-
-(defn over-each-comp-child [parent children]
-  (log/debug (str "over-each-comp-child: parent type: " (type parent)))
-  (log/debug (str "over-each-comp-child: comp children type: " (type children)))
-  (if (not (empty? children))
-    (let [each-child (first children)]
-      (log/debug (str "over-each-comp-child: each-parent?: " (:comment (first children))))
-      (lazy-cat
-       (overc parent each-child)
-       (over-each-comp-child parent (rest children))))
-    (do
-      (log/debug (str "over-each-comp-child: done. returning nil."))
-      nil)))
+;; TODO: move to (over)
+(defn moreover-comp [parent child lexfn-sem-impl]
+  (log/debug (str "moreover-comp parent: " (fo parent)))
+  (log/debug (str "moreover-comp comp:" (fo child)))
+  (log/debug (str "moreover-comp type parent: " (type parent)))
+  (log/debug (str "moreover-comp type comp:" (type child)))
+  (let [result
+        (unifyc parent
+                (unifyc {:comp child}
+                        {:comp {:synsem {:sem (lexfn-sem-impl (get-in child '(:synsem :sem) :top))}}}))]
+    (if (not (fail? result))
+      (let [debug (log/trace (str "moreover-comp (SUCCESS) parent (2x) sem: " (get-in parent '(:synsem :sem))))]
+        (let [result
+              (merge {:comp-filled true}
+                     result)]
+          (log/debug (str "moreover-comp (SUCCESS) merged result:(fo) " (fo result)))
+          )
+          result)
+      (do
+        (log/debug "moreover-comp: fail at: " (fail-path result))
+        (if (and
+             *throw-exception-if-failed-to-add-complement*
+             (get-in child '(:head)))
+          (throw (Exception. (str "failed to add complement: " (fo child) "  to: phrase: " (fo parent)
+                                  ". Failed path was: " (fail-path result)
+                                  ". Value of parent at path is: "
+                                  (get-in parent (fail-path result))
+                                  "; Synsem of child is: "
+                                  (get-in child '(:synsem) :top)))))
+        (log/debug "moreover-comp: complement synsem: " (get-in child '(:synsem) :top))
+        (log/debug "moreover-comp:  parent value: " (get-in parent (fail-path result)))
+        :fail))))
 
 (defn overh [parent head]
   "add given head as the head child of the phrase: parent."
@@ -196,8 +133,10 @@
     (set? parent))
    (let [parents (lazy-seq parent)]
      (filter (fn [result]
-               (not (fail? result)))
-             (over-each-parent-head parents head)))
+               (not (= result '(:fail))))
+             (map (fn [each-parent]
+                    (overh each-parent head))
+                  parents)))
 
    (string? head)
    (overh parent (it head))
@@ -210,8 +149,10 @@
    (let [head-children head]
      (log/debug (str "head is a seq - actual type is " (type head)))
      (filter (fn [result]
-               (not (fail? result)))
-             (over-each-head-child parent head-children)))
+               (not (= result '(:fail))))
+             (map (fn [each-child]
+                    (overh parent each-child))
+                  head-children)))
 
    true
    (do
@@ -246,8 +187,10 @@
     (set? parent))
    (let [parents (lazy-seq parent)]
      (filter (fn [result]
-               (not (fail? result)))
-             (over-each-parent-comp parents comp)))
+               (not (= result '(:fail))))
+             (mapcat (fn [each-parent]
+                       (overc each-parent comp))
+                     parent)))
 
    (string? comp)
    (overc parent (it comp))
@@ -260,8 +203,10 @@
    (let [comp-children comp]
      (log/debug (str "comp is a seq - actual type is " (type comp)))
      (filter (fn [result]
-               (not (fail? result)))
-             (over-each-comp-child parent comp-children)))
+               (not (= result '(:fail))))
+             (mapcat (fn [each-child]
+                       (overc parent each-child))
+                     comp-children)))
 
    true
    (do
