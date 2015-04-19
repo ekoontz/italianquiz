9a123c18080044c06badee926fc785b185896723
Eugene Koontz <ekoontz@hiro-tan.org>
2015-04-19 12:24:50 -0700

    contains credentials in plaintext: squash this after removing them.

--- src/italianverbs/auth/google.clj
+++ src/italianverbs/auth/google.clj
@@ -11,16 +11,22 @@
 
 (derive ::admin ::user)
 
-;(def client-config
-;  {:client-id "946241140791-833sod9itqgm9v8ihi0gj4ca9c3oevcr.apps.googleusercontent.com"
-;   :client-secret "pAD9FWexu96vw8YPH0fbPAoB"
-;   :callback {:domain "http://localhost:3000" :path "/oauth2callback"}})
-
 ;; verbcoach-workstation
-(def client-config
+(def workstation-google-api-config
+;; TODO: checked in to source code: remove here and in Google console and replace with env settings.
   {:client-id "652200734300-3tbdfqhisnlctt6vh70boofrc08qc6a7.apps.googleusercontent.com"
    :client-secret "Nvj41la8ao_wsnXQunbp5arR"
    :callback {:domain "http://localhost:3000" :path "/oauth2callback"}})
+
+;; verbcoach-dev
+;; TODO: checked in to source code: remove here and in Google console and replace with env settings.
+(def verbcoach-dev-google-api-config
+  {:client-id "845033688568-1emrraqe25pnlj984s3cndfpd6s963vh.apps.googleusercontent.com"
+   :client-secret "Y6dqv5fFna2QB4E5Xm-Yz8y3"
+   :callback {:domain "http://verbcoach-dev.herokuapp.com" :path "/oauth2callback"}})
+
+;; TODO: move to environment setting
+(def client-config workstation-google-api-config)
 
 (defn credential-fn [token]
   ;;lookup token in DB or whatever to fetch appropriate :roles
@@ -62,8 +68,6 @@
                     " times.</p><p>The current session: " session "</p>"))
               (assoc :session session))))
 
-
-
    (GET "/login" request
         (do
           (friend/authorize #{::user} "Authorized page.")
@@ -73,5 +77,3 @@
         
    (GET "/admin" request
         (friend/authorize #{::admin} "Only admins can see this page."))))
-
-  
