82e4753d49463bc61b8a994d92bdb894c7937fc5
Eugene Koontz <ekoontz@hiro-tan.org>
2015-04-26 21:50:05 -0400

    fix —ire => isco verb inflection

--- test/italianverbs/test/morphology/italiano.clj
+++ test/italianverbs/test/morphology/italiano.clj
@@ -11,3 +11,20 @@
 (deftest lookup-inflection
   (let [result (it/it "dorme")]
     (is (> (.size result) 0))))
+
+(deftest inflect-parlo
+  (let [result (get-string {:agr {:person :1st
+                                  :number :sing}
+                            :cat :verb
+                            :infl :present
+                            :italiano "parlare"})]
+    (is (= result "parlo"))))
+
+(deftest inflect-isco
+  (let [result (get-string {:agr {:person :1st
+                                  :number :sing}
+                            :boot-stem1 "capisc"
+                            :cat :verb
+                            :infl :present
+                            :italiano "capire"})]
+    (is (= result "capisco"))))
