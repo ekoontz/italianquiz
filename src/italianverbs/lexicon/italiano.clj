5eb4eb3375c7dcec0f0734d467b4d841e951f7fa
Eugene Koontz <ekoontz@hiro-tan.org>
2015-05-13 23:40:58 -0700

    add futuro-stem of caricare

--- src/italianverbs/lexicon/italiano.clj
+++ src/italianverbs/lexicon/italiano.clj
@@ -436,7 +436,8 @@
    "cantare" {:synsem {:cat :verb
                        :sem {:pred :cantare}}}
 
-   "caricare" {:synsem {:cat :verb
+   "caricare" {:italiano {:futuro-stem "carich"}
+               :synsem {:cat :verb
                         :sem {:pred :caricare}}}
 
    "cenare" {:synsem {:cat :verb
