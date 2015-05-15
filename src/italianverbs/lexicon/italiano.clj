fab0f00dbdd0f66c36fcdbd2a9ffb368059bbbfc
Eugene Koontz <ekoontz@hiro-tan.org>
2015-05-14 22:58:39 -0700

    fix typo

--- src/italianverbs/lexicon/italiano.clj
+++ src/italianverbs/lexicon/italiano.clj
@@ -1400,7 +1400,7 @@
                  :synsem {:cat :verb
                           :sem {:pred :giveback-return}}}
 
-   "riconocere" {:system {:cat :verb
+   "riconoscere" {:system {:cat :verb
                           :sem {:pred :recognize}}}
 
    "ricevere"  {:synsem {:cat :verb
