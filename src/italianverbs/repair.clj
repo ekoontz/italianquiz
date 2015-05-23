8df7940d48edfb7f81f0a627ce3f381324bfae58
Eugene Koontz <ekoontz@hiro-tan.org>
2015-05-23 10:10:37 -0700

    use log/debug, not println

--- src/italianverbs/repair.clj
+++ src/italianverbs/repair.clj
@@ -1,5 +1,6 @@
 (ns italianverbs.repair)
 
+(require '[clojure.tools.logging :as log])
 (require '[italianverbs.unify :refer [unify]])
 (require '[italianverbs.italiano :refer [fill-by-spec fill-verb]])
 (require '[italianverbs.english :refer :all])
@@ -15,23 +16,26 @@
                          (do
                            (if (:sql member-of-unit)
                              (do
-                               (println (str "doing sql: " (:sql member-of-unit)))
+                               (log/debug (str "doing sql: " (:sql member-of-unit)))
                                (exec-raw (:sql member-of-unit))))
                            (if (:fill member-of-unit)
                              (do
-                               (println (str "doing fill-by-spec: " (->> member-of-unit :fill :spec)))
+                               (log/debug (str "doing fill-by-spec: " (->> member-of-unit :fill :spec)))
                                (fill-by-spec 10 (->> member-of-unit :fill :spec) 
                                                     "expression_import")))
 
                            (if (:fill-verb member-of-unit)
                              (do
-                               (println (str "doing fill-verb: " (:fill-verb member-of-unit)))
+                               (log/debug (str "doing fill-verb: " (:fill-verb member-of-unit)))
                                (let [verb (:fill-verb member-of-unit)]
                                  (.size (fill-verb verb 10 :top "expression_import")))))))
 
                        unit)))
          units))
-    (exec-raw ["SELECT count(*) FROM expression_import"] :results)
+    
+    (log/info (str "expression_import count:" 
+                   (:count (first (exec-raw ["SELECT count(*) FROM expression_import"]
+                                            :results)))))
 
     (exec-raw "DROP TABLE IF EXISTS expression_distinct")
 
@@ -45,6 +49,10 @@
     (exec-raw "INSERT INTO expression_distinct (language,model,surface,structure,serialized) 
          SELECT DISTINCT language,model,surface,structure,serialized 
                     FROM expression_import")
+
+    (log/info (str "expression_distinct count:" 
+                   (:count (first (exec-raw ["SELECT count(*) FROM expression_distinct"]
+                                            :results)))))
 
     (exec-raw "INSERT INTO expression (language,model,surface,structure,serialized)
                     SELECT language,model,surface,structure,serialized
