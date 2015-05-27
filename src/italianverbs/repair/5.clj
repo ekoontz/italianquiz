978ae59f3e48dc1d623224289e445a83eab3e380
Eugene Koontz <ekoontz@hiro-tan.org>
2015-05-26 22:46:51 -0700

    add repair method to add “lavarsi” sentences

---
+++ src/italianverbs/repair/5.clj
@@ -0,0 +1,23 @@
+;; usage:
+;; export POSTGRES_URL=<target database>
+;; lein run -m italianverbs.repair.5/repair
+(ns italianverbs.repair.5)
+(require '[italianverbs.english :as en])
+(require '[italianverbs.italiano :as it])
+(require '[italianverbs.repair :refer [process]])
+
+;; (fill-by-spec {:root {:italiano {:italiano "lavare"}}} 1 "expression" small-plus-vp-pronoun "small-plus-vp-pronoun")
+
+;; 
+
+(defn repair []
+  (process 
+   [{:fill-verb "lavarsi"
+     :source-model it/small-plus-vp-pronoun
+     :target-model en/small-plus-vp-pronoun}]))
+
+
+
+
+
+
