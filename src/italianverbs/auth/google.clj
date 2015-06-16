19a32916420e8e7709911cf6524a1303475e965a
Eugene Koontz <ekoontz@hiro-tan.org>
2015-06-16 00:17:07 -0700

    Now successfully writing to the ‘session’ table, but only if logged in (TODO: need to do so if not logged in)

--- src/italianverbs/auth/google.clj
+++ src/italianverbs/auth/google.clj
@@ -60,135 +60,146 @@
            (get (:cookies request) "ring-session"))
     (:value (get (:cookies request) "ring-session"))))
 
-(defn update-or-insert-session [access-token user-id request]
-  (let [session-row (first (k/exec-raw ["SELECT session.*
-                                             FROM vc_user
-                                       INNER JOIN authenticated_session AS session
-                                               ON (session.user_id = vc_user.id)
-                                            WHERE vc_user.id=?" [user-id]] :results))
-
-        ring-session (get-in request [:cookies "ring-session" :value])
-
-        debug (log/trace (str "update-or-insert-session: access-token:" access-token))
-        debug (log/debug (str "update-or-insert-session: user-id:" user-id))
-        debug (log/trace (str "update-or-insert-session: ring-session:" ring-session))
-        debug (log/trace (str "update-or-insert-session: request:" request))
-        debug (log/debug (str "update-or-insert-session: cookies:" (get-in request [:cookies])))
-
-        ]
-    (if session-row
-      ;; a session row exists, but if we got here, it does not have the access_token, so set its access_token.
-      ;; TODO: is updating when it does not have to..
-      (do
-        (log/debug (str "updating session: " session-row " with new access-token."))
-        (k/exec-raw [(str "UPDATE authenticated_session SET (access_token,ring_session) = (?,?::uuid) WHERE user_id=? AND access_token != ?")
-                     [access-token ring-session user-id access-token]]))
-      ;; else, no session row, so insert one.
-      (if request
-        (let []
-          (log/debug (str "(token2username) inserting new row in session table; user-id=" user-id))
-          (log/trace (str "(token2username) inserting new row in session table; user-id=" user-id " and access-token=" access-token))
-          (insert-session user-id access-token ring-session)
-          )
-        (do (log/warn (str "(token2username): could not insert new session since no request object was given."))
-            (throw (Exception. "no request given.")))))))
+(defn update-or-insert-session
+  ([session]
+     (let [session-row (first (k/exec-raw ["SELECT session.*
+                                              FROM session 
+                                             WHERE ring_session = ?::uuid" [session]] :results))
+           debug (log/trace (str "update-or-insert-session: ring-session:" session))
+           ]
+       (if (not session-row)
+         (insert-session session))))
+  
+  ([session access-token user-id]
+     (let [sql-only (log/info (str "SQL ONLY: "
+                                   (k/sql-only ["SELECT session.*
+                                                   FROM session 
+                                                  WHERE ring_session = ?::uuid
+                                                    AND access_token = ?
+                                                    AND user_id = ?" [session access-token user-id]])))
+           session-row (first (k/exec-raw ["SELECT session.*
+                                              FROM session 
+                                             WHERE ring_session = ?::uuid
+                                               AND access_token = ?
+                                               AND user_id = ?" [session access-token user-id]] :results))
+           debug (log/trace (str "update-or-insert-session: session:" session))
+           ]
+       (if (not session-row)
+         (insert-session user-id access-token session)))))
 
 ;; TODO: factor out update/insert into separate function with more representative names
 (defn token2username [access-token & [request]]
   "Get user's email address from the vc_user table, given their access token. 
    If access-token is not registered in database,
    ask Google for the user's email address and add as new row to the vc_user table."
-  (let [user-by-access-token
-        (first (k/exec-raw [(str "SELECT email,session.user_id AS userid
+  (cond
+   (nil? access-token)
+   (do
+     (log/error "token2user: supplied access-token was null.")
+     (throw (Exception. (str "token2user: supplied access-token was null."))))
+
+   true
+   (let [debug (log/debug (str "token2username: access-token: " access-token))
+         user-by-access-token
+         (first (k/exec-raw [(str "SELECT email,session.user_id AS userid
                                     FROM vc_user 
-                              INNER JOIN authenticated_session AS session
+                              INNER JOIN session
                                       ON (vc_user.id = session.user_id) 
                                      AND access_token=?")
-                            [access-token]] :results))]
-    (if user-by-access-token
-      (let [email (:email user-by-access-token)
-            user-id (:userid user-by-access-token)
-            ring-session (get-in request [:cookies "ring-session" :value])]
+                             [access-token]] :results))]
+     (if user-by-access-token
+       (let [email (:email user-by-access-token)
+             user-id (:userid user-by-access-token)
+             ring-session (get-in request [:cookies "ring-session" :value])]
+         
+         (log/info (str "found user by access-token in Postgres vc_user database: email: "
+                        email))
+         (if request (do
+                       (log/trace (str "found user by access-token, and currently the request is: " request))
+                       (log/debug (str "found user by access-token, and currently the ring-session (from request) is: " ring-session))
+                       (update-or-insert-session (get-in request [:cookies "ring-session" :value]) access-token user-id)))
+         email)
 
-        (log/info (str "found user by access-token in Postgres vc_user database: email: "
-                       email))
-        (if request (do
-                      (log/trace (str "found user by access-token, and currently the request is: " request))
-                      (log/debug (str "found user by access-token, and currently the ring-session (from request) is: " ring-session))
-                      (update-or-insert-session access-token user-id request)))
-        email)
+       ;; else, user could not be found locally by searching the 'session' table access token, so get user's info from google by using the access-token.
+       (do
+         (log/info (str "user's token was not in database: querying: https://www.googleapis.com/oauth2/v1/userinfo?access_token=<input access token> to obtain user info"))
+         (let [{:keys [status headers body error] :as resp} 
+               @(http/get 
+                 (str "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" access-token))]
+           (cond error
+                 (log/info "Failed, exception: " error)
+                 
+                 (not (= status 200))
+                 (do
+                   (log/error (str "Received a non-200 status from Google: status=" status))
+                   (log/error (str "Received a non-200 status from Google: body=" body))
+                   nil)
+                 
+                 true
+                 (do
+                   (log/debug (str "body: " body))
+                   (let [body (json/read-str body
+                                             :key-fn keyword
+                                             :value-fn (fn [k v]
+                                                         v))
+                         email (get body :email)
+                         given-name (get body :given_name)
+                         family-name (get body :family_name)
+                         picture (get body :picture)]
+                     (log/info (str "Google says user's email is: " email))
+                     (log/info (str "Google says user's given_name is: " given-name))
+                     (log/info (str "Google says user's family_name is: " family-name))
+                     (log/info (str "Google says user's picture is: " picture))
 
-      ;; else, user could not be found locally by searching the 'session' table access token, so get user's info from google by using the access-token.
-      (do
-        (log/info (str "user's token was not in database: querying: https://www.googleapis.com/oauth2/v1/userinfo?access_token=<input access token> to obtain user info"))
-        (let [{:keys [status headers body error] :as resp} 
-              @(http/get 
-                (str "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" access-token))]
-          (cond error
-                (log/info "Failed, exception: " error)
-
-                (not (= status 200))
-                (do
-                  (log/error (str "Received a non-200 status from Google: status=" status))
-                  (log/error (str "Received a non-200 status from Google: body=" body))
-                  nil)
-                
-                true
-                (do
-                  (log/debug (str "body: " body))
-                  (let [body (json/read-str body
-                                            :key-fn keyword
-                                            :value-fn (fn [k v]
-                                                        v))
-                        email (get body :email)
-                        given-name (get body :given_name)
-                        family-name (get body :family_name)
-                        picture (get body :picture)]
-                    (log/info (str "Google says user's email is: " email))
-                    (log/info (str "Google says user's given_name is: " given-name))
-                    (log/info (str "Google says user's family_name is: " family-name))
-                    (log/info (str "Google says user's picture is: " picture))
-
-                    ;; insert a new user record if necessary.
-                    (let [user-id (:id (first (k/exec-raw [(str "SELECT id 
+                     ;; insert a new user record if necessary.
+                     (let [user-id (:id (first (k/exec-raw [(str "SELECT id 
                                                                    FROM vc_user 
                                                                   WHERE email=?")
-                                                           [email]] :results)))
-                          user-id (if user-id
-                                    (do
-                                      (log/info (str "found existing user record with email=" email ";user-id=" user-id))
-                                      user-id)
+                                                            [email]] :results)))
+                           user-id (if user-id
+                                     (do
+                                       (log/info (str "found existing user record with email=" email ";user-id=" user-id))
+                                       user-id)
                                     
-                                    (do
-                                      (log/info (str "inserting new user record."))
-                                      (:id
-                                       (first
-                                        (k/exec-raw [(str "INSERT INTO vc_user (given_name,family_name,picture,updated,email)
+                                     (do
+                                       (log/info (str "inserting new user record."))
+                                       (:id
+                                        (first
+                                         (k/exec-raw [(str "INSERT INTO vc_user (given_name,family_name,picture,updated,email)
                                                         VALUES (?,?,?,now(),?) RETURNING id")
-                                                     [given-name family-name picture email]] :results)))))]
+                                                      [given-name family-name picture email]] :results)))))]
               
-                      ;; insert or update session based on this user_id.
-                      (let [session-row (first (k/exec-raw ["SELECT session.*
+                       ;; insert or update session based on this user_id.
+                       (let [session-row (first (k/exec-raw ["SELECT session.*
                                                                FROM vc_user
-                                                         INNER JOIN authenticated_session AS session
+                                                         INNER JOIN session
                                                                  ON (session.user_id = vc_user.id)
                                                               WHERE vc_user.id=?" [user-id]] :results))
 
-                            ring-session (get-in request [:cookies "ring-session" :value])
-                            ]
-                        (update-or-insert-session access-token user-id request))
-                      email)))))))))
+                             ring-session (get-in request [:cookies "ring-session" :value])
+                             ]
+                         (update-or-insert-session (get-in request [:cookies "ring-session" :value]) access-token user-id))
+                                                  
+                       email))))))))))
 
-(defn insert-session [user-id access-token ring-session]
-  (log/info (str "inserting new session record."))
-  (k/exec-raw [(str "INSERT INTO authenticated_session (access_token,user_id,ring_session)
-                          VALUES (?,?,?::uuid)")
-               [access-token user-id ring-session]]))
+(defn insert-session
+  ([ring-session]
+     (log/info (str "inserting new session record (session=" ring-session))
+     (k/exec-raw [(str "INSERT INTO session (ring_session)
+                             VALUES (?::uuid)")
+                  [ring-session]]))
+  ([user-id access-token ring-session]
+     (log/info (str "inserting new session record (session=" ring-session))
+     (log/info (str "                             (access-token=" access-token))
+     (log/info (str "                             (user-id=" user-id))
+     (k/exec-raw [(str "INSERT INTO session (access_token,user_id,ring_session)
+                             VALUES (?,?,?::uuid)")
+                  [access-token user-id ring-session]])))
 
 (defn token2info [access-token]
   (first (k/exec-raw [(str "SELECT vc_user.* 
                               FROM vc_user
-                        INNER JOIN authenticated_session AS session
+                        INNER JOIN session
                                 ON (vc_user.id = session.user_id)
                              WHERE access_token=?")
                       [access-token]]
@@ -221,13 +232,15 @@
               (log/error (str "No google client callback was found in the environment."))
               (throw (Exception. (str "Administrator must define GOOGLE_CALLBACK_DOMAIN in my runtime environment.")))))
 
+
           (friend/authorize #{::user} "Authorized page.")
+          
           (is-authenticated
            (do
              (let [username (token2username
                              (-> request :session :cemerick.friend/identity :current :access-token)
                              request)]
-               (log/debug (str "Logging in user with access token: " 
+               (log/debug (str "Logging in user: " username " with access token: " 
                                (-> request :session :cemerick.friend/identity :current :access-token))))
              {:status 302
               :headers {"Location" "/"}}))))
