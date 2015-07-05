94d75e1697f977d24a2ef13ae7855111e2d37a6e
Eugene Koontz <ekoontz@hiro-tan.org>
2015-07-05 00:49:29 -0700

    show most recent questions answered

--- src/italianverbs/me.clj
+++ src/italianverbs/me.clj
@@ -5,9 +5,11 @@
    [clojure.string :as string]
    [clojure.tools.logging :as log]
    [compojure.core :as compojure :refer [GET PUT POST DELETE ANY]]
+   [italianverbs.config :refer [time-format]]
    [italianverbs.html :refer [page]]
    [korma.core :as k]))
 
+(declare latest-questions)
 (declare me)
 (declare profile-table)
 
@@ -26,24 +28,88 @@
                   :jss ["/css/me.js"]})}))))
 
 (defn me [request]
-  (let [profile {}]
-    [:div#me {:class "major"}
+  (let [profile {:verb "parlare"
+                 :tense :present
+                 }]
+    [:div#me
+    
+     [:div#myprofile {:class "major"}
 
-     [:h2 "My profile"]
+      [:h2 "Profile"]
 
-     [:h3 "Overall"]
+      [:h3 "Overall"]
      
+      (profile-table profile)
+      
+      [:h3 "The foo game"]
+      
+      (profile-table profile)
+      
+      [:h3 "The bar game"]
+      
      (profile-table profile)
+      ]
 
-     [:h3 "The foo game"]
-     
-     (profile-table profile)
-     
-     [:h3 "The bar game"]
-     
-     (profile-table profile)
 
+     [:div#last {:class "major"}
+      [:h2 "Latest questions"]
+      (latest-questions)      
+      ]
      ]))
+
+(defn latest-questions []
+  (let [query
+        "SELECT to_char(issued,?) AS issued,
+                issued AS issued_sortby,
+                expression.surface AS question,answer,
+                time_to_correct_response AS ttcr
+           FROM question 
+     INNER JOIN expression 
+             ON (expression.id = question.source) 
+       ORDER BY issued_sortby DESC 
+          LIMIT 50"
+
+        results (k/exec-raw [query [time-format]]
+                            :results)]
+   (if (empty? results)
+     [:div [:i "None."]]
+
+     ;; else
+     [:table {:class "striped padded"}
+      [:tr
+       [:th "When"]
+       [:th "Question"]
+       [:th "Answer"]
+       [:th "Profile"]
+       ]
+      
+      (map
+       (fn [result]
+         (let [ttcr (:ttcr result)
+               profile
+               (cond
+                (or (nil? ttcr)
+                    (> ttcr 20000)) ;; really bad.
+                "level0"
+
+                (> ttcr 10000)
+                "level1"
+
+                (> ttcr 5000)
+                "level2"
+
+                (> ttcr 2500)
+                "level3"
+
+                true ;; really good!
+                "level4")]
+           [:tr
+            [:td (:issued result)]
+            [:td (:question result)]
+            [:td (:answer result)]
+            [:td {:class profile}
+             (:ttcr result)]]))
+       results)])))
 
 (defn profile-table [profile]
   [:table.profile
