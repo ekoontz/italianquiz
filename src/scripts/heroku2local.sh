13a33a026009debe9b992e7d58e05a2fc6d308c1
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-09 22:31:33 -0700

    drop db before restoring from backup

--- src/scripts/heroku2local.sh
+++ src/scripts/heroku2local.sh
@@ -1,3 +1,4 @@
 curl -o latest.dump $(heroku pg:backups public-url --app verbcoach)
+dropdb verbcoach_restore
 createdb verbcoach_restore
 pg_restore --verbose --clean --no-acl --no-owner -U verbcoach -d verbcoach_restore latest.dump
