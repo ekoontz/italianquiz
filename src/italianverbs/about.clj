c888ee77488c86ed80a8a59ae780cbbb088ad8a0
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-11 23:56:08 -0700

    show per-city list of games

--- src/italianverbs/about.clj
+++ src/italianverbs/about.clj
@@ -3,6 +3,7 @@
    [hiccup core page])
   (:require
    [cemerick.friend :as friend]
+   [clojure.string :as string]
    [compojure.core :refer [GET]]
    [environ.core :refer [env]]
    [italianverbs.menubar :refer [menubar]]
@@ -12,7 +13,8 @@
                               login-form
                               menubar-info-for-user
                               ]]
-   [italianverbs.html :as html]))
+   [italianverbs.html :as html]
+   [korma.core :as k]))
 
 (declare about)
 
@@ -38,25 +40,129 @@
 
     [:div.intro "The best place on the web to learn how to conjugate verbs."]
     [:div.flags
-     [:div.flag
-       [:img {:src "/png/Flag_of_Italy.svg.png" }]
-       [:div.language "Italiano"]
-      ]
+     [:table
+      [:tr
+       [:th
+        [:div.flag
+         [:img {:src "/png/Flag_of_Italy.svg.png" }]]]
 
-     [:div.flag
-       [:img {:src "/png/Flag_of_Mexico.svg.png" }]
-       [:div.language "Español"]
-      ]
+       [:th
+        [:div.flag
+         [:img {:src "/png/Flag_of_Mexico.svg.png" }]
+         ]
+        ]
 
-     [:div.flag
-       [:img {:src "/png/Flag_of_Spain.svg.png" }]
-       [:div.language "Español"]
-      ]
-     
+       [:th
+        [:div.flag
+         [:img {:src "/png/Flag_of_Spain.svg.png" }]
+         ]
+        ]
 
-     [:div.flag
-      [:img {:src "/png/Flag_of_France.svg.png" }]
-      [:div.language "Français"]
-      ]
-     ]
-    ])
+       [:th
+        [:div.flag
+         [:img {:src "/png/Flag_of_France.svg.png" }]
+         ]
+        ]
+       ]
+
+      [:tr
+       [:td "Italiano"]
+       [:td "Español"]
+       [:td "Español"]
+       [:td "Français"]
+       ]
+    
+      [:tr
+       [:td "Firenze"]
+       [:td "México D.F."]
+       [:td "Barcelona"]
+       [:td "Paris"]
+       ]
+
+      [:tr       
+       [:td
+        [:ul
+         (let [results
+               (k/exec-raw
+               ["SELECT id,name FROM game WHERE city=? LIMIT 5" ["Firenze"]] :results)]
+           (map (fn [result]
+                  (let [name (:name result)]
+                    [:li
+                     [:a {:title name
+                          :href (str "/tour/" (:id result))}
+                      (str
+                       (subs name 0 (min 12 (.length name)))
+                       (cond (> (.length name) 12)
+                            "..."
+                            (= (.length name) 0)
+                            "(unnamed game)"
+                            true
+                            ""))]]))
+                results))
+         ]]
+
+       [:td
+        [:ul
+         (let [results
+               (k/exec-raw
+                ["SELECT id,name FROM game WHERE city=? LIMIT 5" ["México D.F."]] :results)]
+           (map (fn [result]
+                  (let [name (:name result)]
+                    [:li
+                     [:a {:title name
+                          :href (str "/tour/" (:id result))}
+                      (str
+                       (subs name 0 (min 12 (.length name)))
+                       (cond (> (.length name) 12)
+                             "..."
+                             (= (.length name) 0)
+                             "(unnamed game)"
+                             true
+                             ""))]]))
+                results))
+         ]]
+
+
+       [:td
+        [:ul
+         (let [results
+               (k/exec-raw
+                ["SELECT id,name FROM game WHERE city=? LIMIT 5" ["Barcelona"]] :results)]
+           (map (fn [result]
+                  (let [name (:name result)]
+                    [:li
+                     [:a {:title name
+                          :href (str "/tour/" (:id result))}
+                      (str
+                       (subs name 0 (min 12 (.length name)))
+                       (cond (> (.length name) 12)
+                             "..."
+                             (= (.length name) 0)
+                             "(unnamed game)"
+                             true
+                             ""))]]))
+                results))
+         ]]
+
+       [:td
+        [:ul
+         (let [results
+               (k/exec-raw
+                ["SELECT id,name FROM game WHERE city=? LIMIT 5" ["Paris"]] :results)]
+           (map (fn [result]
+                  (let [name (:name result)]
+                    [:li
+                     [:a {:title name
+                          :href (str "/tour/" (:id result))}
+                      (str
+                       (subs name 0 (min 12 (.length name)))
+                       (cond (> (.length name) 12)
+                             "..."
+                             (= (.length name) 0)
+                             "(unnamed game)"
+                             true
+                             ""))]]))
+                results))
+         ]]
+       ]
+      ]]])
