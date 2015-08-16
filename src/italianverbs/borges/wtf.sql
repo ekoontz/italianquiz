b6b457b5cd19c0df5b14ff96604c9a31c483737d
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-16 12:54:13 -0700

    cleanup

--- src/italianverbs/borges/wtf.sql
+++
@@ -1,9 +0,0 @@
-SELECT target.serialized::text AS target,target.surface
-	                                        FROM expression AS target
-
-	                                  INNER JOIN expression AS source
-	                                          ON (source.structure->'synsem'->'sem') @> (target.structure->'synsem'->'sem')
-	                                         AND (source.language='en')
-
-	                                       WHERE target.language='it'
-	                                         AND (target.structure @> '{"synsem":{"subcat":[]}}')
