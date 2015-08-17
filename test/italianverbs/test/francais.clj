c496cabff7fa51bab7dce7bd780f40a458077f6b
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-16 23:35:50 -0700

    cleanup

--- test/italianverbs/test/francais.clj
+++
@@ -1,8 +0,0 @@
-(ns italianverbs.test.francais)
-(require '[clojure.test :refer :all])
-(require '[italianverbs.francais :as fr])
-(require '[italianverbs.morphology.francais :refer [fo]])
-
-(deftest future-tense
-  (is (not (empty? (filter #(= "j'accompagnerai" %)
-                           (map fo (fr/+ "je" "accompagner")))))))
