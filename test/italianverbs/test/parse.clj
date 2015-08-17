f92f823f94d27d5d31ff4d416a05e8abcdea084e
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-16 23:44:44 -0700

    more cleanup

--- test/italianverbs/test/parse.clj
+++
@@ -1,7 +0,0 @@
-(ns italianverbs.test.parse
-  (:refer-clojure :exclude [get-in merge resolve find]))
-
-(require '[clojure.test :refer :all])
-(require '[clojure.string :as str])
-
-;; TODO: add some language-independent parsing tests
