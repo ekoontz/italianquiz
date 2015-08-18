298ec755273c0daffa3837c806911eb22d006ee5
Eugene Koontz <ekoontz@hiro-tan.org>
2015-08-17 19:18:58 -0700

    1. if not logged in, direct user to login 2. if no teacher, direct user to search for teacher

--- src/italianverbs/about.clj
+++ src/italianverbs/about.clj
@@ -9,7 +9,7 @@
    [environ.core :refer [env]]
    [italianverbs.menubar :refer [menubar]]
    [italianverbs.session :as session]
-   [italianverbs.user :refer [do-if do-if-teacher haz-admin?
+   [italianverbs.user :refer [do-if do-if-authenticated do-if-teacher haz-admin?
                               haz-auth? has-admin-role has-teacher-role
                               login-form
                               menubar-info-for-user
@@ -18,6 +18,7 @@
    [korma.core :as k]))
 
 (declare about)
+(declare check-for-teacher)
 
 (defn resources [request]
   {:css ["/css/about.css"]
@@ -27,59 +28,54 @@
 (def routes 
   (compojure/routes
    (GET "/about" request
-        {:status 200
-         :headers {"Content-Type" "text/html;charset=utf-8"}
-         :body (html/page "Welcome to Verbcoach"
-                          (about request)
-                          request
-                          resources)})
+        (do-if-authenticated
+         {:status 200
+          :headers {"Content-type" "text/html;charset=utf-8"}
+          :body (html/page "Welcome to Verbcoach"
+                           (check-for-teacher request)
+                           request
+                           resources)}
+         {:status 200
+          :headers {"Content-type" "text/html;charset=utf-8"}
+          :body (html/page "Welcome to Verbcoach"
+                           (html [:h3 "Please login to choose your teacher."])
+                           request
+                           resources)}))
+          
+
+         
    (POST "/about" request
          {:status 302
           :headers {"Location" "/about"}})))
 
-(defn under-a-city [city search-term]
-  (let [truncate-game-name 50]
-    [:td
-     [:ul
-      (let [results
-            (k/exec-raw
-           ["SELECT game.id,game.name,
-                    teacher.given_name || ' ' || teacher.family_name AS teacher_name,
-                    teacher.email AS teacher_email
-               FROM game
-         INNER JOIN vc_user AS teacher
-                 ON game.created_by = teacher.id
-              WHERE ((name ILIKE ?)
-                     OR (city ILIKE ?)
-                     OR  (teacher.given_name ILIKE ?)
-                     OR  (teacher.email ILIKE ?)
-                     OR  (? = ''))
-                AND (game.active = true)
-                AND (city=?)"
-            [(str "%" search-term "%")
-             (str "%" search-term "%")
-             (str "%" search-term "%")
-             (str "%" search-term "%")
-             search-term
-             city]] :results)]
-        (map (fn [result]
-               (let [name (str (:name result) " : " 
-                               (if (:teacher_name result)
-                                 (:teacher_name result)
-                                 (:teacher_email result)))]
-                 [:li
-                  [:a {:title name
-                       :href (str "/tour/" (:id result))}
-                   (str
-                    (subs name 0 (min truncate-game-name (.length name)))
-                    (cond (> (.length name) truncate-game-name)
-                          "..."
-                             (= (.length name) 0)
-                             "(unnamed game)"
-                             true
-                             ""))]]))
-             results))
-      ]]))
+(defn check-for-teacher [request]
+  [:div.major
+   [:h2 "Welcome to Verbcoach."]
+   [:h3 "Please find your teacher."]
+   
+   (let [search-term (:search (:params request))
+         results
+         (if (= search-term "")
+           []
+           (k/exec-raw
+            ["SELECT teacher.given_name || ' ' || teacher.family_name AS teacher_name
+                FROM vc_user AS teacher
+                WHERE (teacher.family_name ILIKE ?)"
+             [(str "%" search-term "%")]] :results))]
+     [:div {:style "float:left"}
+      [:div.search_form
+       [:form {:method "get"}
+        [:input {:name "search" :size "50" :value search-term}
+         ]
+        [:button {:onclick "submit()"} "Search"]
+     
+        ]
+       ]
+
+      [:div.rows2table
+       (html/rows2table results)]])])
+
+(declare under-a-city)
 
 (defn about [request]
   (let [truncate-game-name 50
@@ -145,4 +141,52 @@
     )
 
   )
+
+(defn under-a-city [city search-term]
+  (let [truncate-game-name 50]
+    [:td
+     [:ul
+      (let [results
+            (if (= search-term "")
+              []
+            (k/exec-raw
+           ["SELECT game.id,game.name,
+                    teacher.given_name || ' ' || teacher.family_name AS teacher_name,
+                    teacher.email AS teacher_email
+               FROM game
+         INNER JOIN vc_user AS teacher
+                 ON game.created_by = teacher.id
+              WHERE ((name ILIKE ?)
+                     OR (city ILIKE ?)
+                     OR  (teacher.given_name ILIKE ?)
+                     OR  (teacher.family_name ILIKE ?)
+                     OR  (teacher.email ILIKE ?)
+                     OR  (? = ''))
+                AND (game.active = true)
+                AND (city=?)"
+            [(str "%" search-term "%")
+             (str "%" search-term "%")
+             (str "%" search-term "%")
+             (str "%" search-term "%")
+             (str "%" search-term "%")
+             search-term
+             city]] :results))]
+        (map (fn [result]
+               (let [name (str (:name result) " : " 
+                               (if (:teacher_name result)
+                                 (:teacher_name result)
+                                 (:teacher_email result)))]
+                 [:li
+                  [:a {:title name
+                       :href (str "/tour/" (:id result))}
+                   (str
+                    (subs name 0 (min truncate-game-name (.length name)))
+                    (cond (> (.length name) truncate-game-name)
+                          "..."
+                             (= (.length name) 0)
+                             "(unnamed game)"
+                             true
+                             ""))]]))
+             results))
+      ]]))
 
